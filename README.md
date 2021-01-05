ee660243f884187a0580e451556332a72954fd8c
Florens Verschelde <florens@fvsch.com>
2021-01-05 14:46:32 +0100

    Update dependencies

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # precision-click-game
 
-Small game developed as a demonstration of Fitt's Law for [24 jours de web](https://www.24joursdeweb.fr/).
+Small game developed as a demonstration of Fitt’s Law for [24 jours de web](https://www.24joursdeweb.fr/).
+
+Updated for [this article on Learning XState](https://fvsch.com/learning-xstate).
 
 ## Get started
 
--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import replace from "@rollup/plugin-replace";
+import css from 'rollup-plugin-css-only';
 import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
 import svelte from "rollup-plugin-svelte";
@@ -21,7 +22,6 @@ function serve() {
         stdio: ["ignore", "inherit", "inherit"],
         shell: true,
       });
-
       process.on("SIGTERM", toExit);
       process.on("exit", toExit);
     },
@@ -38,13 +38,15 @@ export default {
   },
   plugins: [
     svelte({
-      // enable run-time checks when not in production
-      dev: !production,
-      // we'll extract any component CSS out into
-      // a separate file - better for performance
-      css: (css) => {
-        css.write("bundle.css");
+      compilerOptions: {
+        // enable run-time checks when not in production
+        dev: !production,
       },
+    }),
+    // we'll extract any component CSS out into
+    // a separate file - better for performance
+    css({
+      output: "bundle.css"
     }),
 
     // If you have external dependencies installed from
